c48122b355413bfb2c8a0123f9ec9163c16ef52d
William Maitre <william.maitre@gmail.com>
2022-08-31 11:07:05 -0400

    updated the exercise

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -21,5 +21,12 @@ namespace Testing.Controllers
 
             return View(products);
         }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+
+            return View(product);
+        }
     }
 }
--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -9,5 +9,6 @@ namespace Testing
     public interface IProductRepository
     {
         public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
     }
 }
