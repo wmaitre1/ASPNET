3d71f597047ba22ed87843d3450bcbc71b8e8859
William Maitre <william.maitre@gmail.com>
2022-08-31 09:52:22 -0400

    deleted appsettings.json and finished part one

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
