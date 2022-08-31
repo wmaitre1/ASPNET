a50d2fb3379284a0d300fee1f5f7ebe8ada7402c
William Maitre <william.maitre@gmail.com>
2022-08-31 11:48:51 -0400

    finished exercise

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -10,5 +10,12 @@ namespace Testing
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+
     }
 }
--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Testing.Models
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
 {
     public class Product
     {
@@ -12,5 +14,6 @@
         public int OnSale { get; set; }
 
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
